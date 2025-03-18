f64c51202af98f78d72e64495565f910fe1ec023
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-03-18 21:35:02 +0100

    Created some routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Une route qui retourne une vue
+Route::get('/about', function () {
+    return view('about');
+});
+
+// Une route qui retourne une chaîne de caractères
+Route::get('/about-string', function () {
+    return 'About page';
+});
+
+// Une route qui retourne un tableau
+Route::get('/about-array', function () {
+    return ['name' => 'John', 'age' => 30];
+});
