55ab8d5bf59e16f622767bb0a09b327880fbfdd8
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-03-25 17:11:36 +0100

    Created some models & migration.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,41 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'PHP Developer',
-                'salary' => 120000,
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Python Developer',
-                'salary' => 130000,
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Java Developer',
-                'salary' => 140000,
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    // Sert à dire à Laravel d'utiliser la table 'job_listings' plutôt que 'jobs' car la table 'jobs' existe déjà
+    protected $table = 'job_listings';
 }
